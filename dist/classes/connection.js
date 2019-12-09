a1f1125bd1426c2c7066f2b531566cf787ad2b84
Milton Hernández <171122@ids.upchiapas.edu.mx>
2019-12-09 10:04:35 -0600

    add travis

--- dist/classes/connection.js
+++ dist/classes/connection.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Pool = require('pg').Pool;
-exports.pool = new Pool({
-    user: 'admin',
-    password: 'root',
-    host: '192.168.0.17',
-    database: 'autobuses',
-    port: 5432,
-});
 // export const pool = new Pool({
-//     user: 'zmkoavjirqmpqp',
-//     password: 'bc5da29f9f22138bb2aee612df66cfcff1b1513c1c54a28d14e3a834b1ada157',
-//     host: 'ec2-174-129-253-146.compute-1.amazonaws.com',
-//     database: 'dmsdoffu6u73v',
+//     user: 'admin',
+//     password: 'root',
+//     host: '192.168.0.17',
+//     database: 'autobuses',
 //     port: 5432,
 // });
+exports.pool = new Pool({
+    user: 'postgres',
+    password: 'MiltonHernandez08.',
+    host: 'boletosdb.cwpktqj9jiw2.us-east-1.rds.amazonaws.com',
+    database: 'boletos',
+    port: 5432,
+});
